4e6c4d2da0184461144896a190c97d16ea7a443f
sheyax <olusheyax@gmail.com>
2021-04-11 18:25:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ try {
     //Server settings
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER; 
     $mail->SMTPDebug = 2;                     //Enable verbose debug output
-    //$mail->isSMTP();                                            //Send using SMTP
+    $mail->isMAIL();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'ayosheyax@gmail.com';                     //SMTP username
