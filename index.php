08456ce83f2d3c9ab7702fc7aa29a291b28e3598
sheyax <olusheyax@gmail.com>
2021-04-11 18:18:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -45,8 +45,9 @@ $mail = new PHPMailer(true);
     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    $mail->send();
+   if( $mail->send()){
     echo 'Message has been sent';
-  catch (Exception $e) {
+   }
+  else {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
